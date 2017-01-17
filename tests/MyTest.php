cb074edfda0b5fe39a0a74f7d3aaeed429a12036
polly3d <polly3d@126.com>
2017-01-17 17:49:41 +0800

    init phpunit test

---
+++ tests/MyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/1/17
+ * Time: 17:30
+ */
+class MyTest extends PHPUnit_Framework_TestCase
+{
+    public function testHi()
+    {
+        $this->assertEquals(true,false);
+    }
+}
---
+++ tests/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/1/17
+ * Time: 17:47
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testFuck()
+    {
+        $this->assertEquals(true,false);
+    }
+}
